b6f5bce8e2d56cdf12f7089e4b2ebe4ce78c2703
parkervoit <parker.r.voit@gmail.com>
2021-03-23 16:43:43 -0500

    updated and modified db & tables exercises

--- mysql_dbexercises.sql
+++ mysql_dbexercises.sql
@@ -1,5 +1,10 @@
+--2
 SHOW DATABASES;
+--3
 USE fruits_db;
+--4
 SELECT database();
+--5
 USE florence17;
+--6
 SELECT database();
--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,13 +1,20 @@
+--3
+USE employees;
+--4
 SHOW TABLES;
+--5-8
 DESCRIBE employees;
+--9
 DESCRIBE dept_emp;
+--10
 SHOW CREATE TABLE dept_manager;
 
-6. emp_no
-7. first_name, last_name
+/*6. emp_no
+7. first_name, last_name, gender
 8.birth_date, hire_date
-9. It shows date types for when each employee began working in that department
-10.CREATE TABLE `dept_manager` (
+9. the emp_no, because it is contained in both of them
+10.
+CREATE TABLE `dept_manager` (
   `emp_no` int(11) NOT NULL,
   `dept_no` char(4) NOT NULL,
   `from_date` date NOT NULL,
@@ -16,4 +23,4 @@ SHOW CREATE TABLE dept_manager;
   KEY `dept_no` (`dept_no`),
   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1*/
