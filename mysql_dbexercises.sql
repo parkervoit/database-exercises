1a4d299d24c9e0e3c77c0f20a5b7520d46c4ce7e
parkervoit <parker.r.voit@gmail.com>
2021-03-23 15:27:44 -0500

    added MySQL database exercise and Tables exercise

---
+++ mysql_dbexercises.sql
@@ -0,0 +1,5 @@
+SHOW DATABASES;
+USE fruits_db;
+SELECT database();
+USE florence17;
+SELECT database();
---
+++ tables_exercises.sql
@@ -0,0 +1,19 @@
+SHOW TABLES;
+DESCRIBE employees;
+DESCRIBE dept_emp;
+SHOW CREATE TABLE dept_manager;
+
+6. emp_no
+7. first_name, last_name
+8.birth_date, hire_date
+9. It shows date types for when each employee began working in that department
+10.CREATE TABLE `dept_manager` (
+  `emp_no` int(11) NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
