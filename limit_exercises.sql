ab252aff729498c7a150a52333260999307726c3
parkervoit <parker.r.voit@gmail.com>
2021-03-25 10:48:55 -0500

    added limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,36 @@
+--2
+SELECT DISTINCT title 
+FROM titles
+LIMIT 10;
+/*  Senior Engineer
+    Staff
+    Engineer
+    Senior Staff
+    Assistant Engineer
+    Technique Leader
+    Manager*/
+--3
+SELECT first_name, last_name, emp_no, hire_date 
+FROM employees 
+WHERE birth_date LIKE '%12-25' AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY hire_date
+LIMIT 5;
+/* the 5 employees are:
+    Alselm	Cappello
+    Utz	Mandell
+    Bouchung	Schreiter
+    Baocai	Kushner
+    Petter	Stroustrup*/
+--4
+SELECT first_name, last_name, emp_no, hire_date 
+FROM employees 
+WHERE birth_date LIKE '%12-25' AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY hire_date
+LIMIT 5 OFFSET 45;
+/* the tenth page of employees is:
+    Pranay	Narwekar
+    Marjo	Farrow
+    Ennio	Karcich
+    Dines	Lubachevsky
+    Ipke	Fontan 
+   Limit sets how many results per page and ultimately the page "length", offset is how you set which page to start at */
